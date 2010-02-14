fb365290f959b36b7a38efa418545acd04c91377
Daniel Truemper <truemped@googlemail.com>
2010-02-14 15:39:10 +0100

    tag cloud, new entries and the list of newest entries are working

--- _attachments/js/shelf.js
+++ _attachments/js/shelf.js
@@ -1,15 +1,31 @@
 
+/*
+ * different types of available items.
+ */
 var types = {
     "book" : {
-        divSelector : "#bookDetails",
-        formSelector : "#bookDetailsForm",
+        name : "Buch",
+        divSelector : "bookDetails",
+        formSelector : "bookDetailsForm",
         fields : [ "title", "authors", "publisher", "year", "tags" ],
+        fieldNames : {
+            "title" : "Titel",
+            "authors" : "Autor",
+            "publisher" : "Verlag",
+            "year" : "Jahr",
+            "tags" : "Schlagwörter"
+        },
         template : { type : "book" },
     }
 };
 
-var newestItemsMustache = "<p><a class=\"link\" rel=\"history\" href=\"#docId{{docid}}\">{{title}}, {{author}}</a></p>";
+var empyDivMustache = "<div id=\"{{divid}}\"></div>";
 
+/* Begin:
+ *
+ * Methods for preserving the correct history.
+ *
+ */
 function initHistory() {
     $.historyInit( historyPreservingClick, "index.html" );
 
@@ -30,106 +46,154 @@ function historyPreservingClick( post_url ) {
     if( post_url ) {
         if( post_url.indexOf( '#newest' ) > -1 ) {
             showNewestItems();
+        } else if( post_url.indexOf( '#new/' ) > -1 ) {
+            renderAndBindDocToForm( { type : post_url.replace( /^.*#new\//, '' ), _id : null } );
         } else if( post_url.indexOf( '#docId' ) > -1 ) {
             var docId = post_url.replace(/^.*#docId/, '');
             showItemDetails( docId );
+        } else if( post_url.indexOf( '#tagcloud' ) > -1 ) {
+            showTagCloud();
+        } else if( post_url.indexOf( '#tag/' ) > -1 ) {
+            var tags = post_url.replace(/^.#tag\//, '').split('\/');
+            showItemsByTags( tags );
         }
     }
 
     $('body').css('cursor','default');
 }
 
-function disableAllDisplays() {
-    $("#content").empty();
+var newDocTypeListMustache = [ "{{#typedesc}}",
+    "<li><a href=\"#new/{{{id}}}\" rel=\"history\">{{name}}</a></li>",
+    "{{/typedesc}}"].join('\n');
+
+function initDocTypes() {
+    var typedesc = new Array();
+    for( var idx in types ) {
+        typedesc.push( { id : idx, name : types[idx]['name'] } );
+    }
+    $("#newDocTypeList").append( $.mustache( newDocTypeListMustache, { typedesc : typedesc } ) );
 }
 
-var bookDetailsHTML = [ "<div id=\"bookDetails\" style=\"display:none\">",
-    "<form id=\"bookDetailsForm\" method=\"post\">",
-    "<table border=\"0\">",
+/* End:
+ *
+ * Methods for preserving the correct history.
+ *
+ */
+
+/* Begin:
+ *
+ * Methods and variables regarding item details.
+ *
+ */
+var itemDetailsMustacheHead = [ "<div id=\"{{divId}}\">",
+    "<form id=\"{{formId}}\" method=\"post\">",
+    "<table border=\"0\">"].join('\n');
+
+var itemDetailsMustasheRows = [ "{{#fields}}",
     "    <tr>",
-    "        <td>Titel</td>",
-    "        <td><input type=\"text\" size=\"50\" name=\"title\" value=\"\" /></td>",
+    "        <td>{{name}}</td>",
+    "        <td><input type=\"text\" size=\"50\" name=\"{{id}}\" value=\"\" /></td>",
     "    </tr>",
-    "    <tr>",
-    "        <td>Autoren</td>",
-    "        <td><input type=\"text\" size=\"50\" name=\"authors\" value=\"\" /></td>",
-    "    </tr>",
-    "    <tr>",
-    "        <td>Verlag</td>",
-    "        <td><input type=\"text\" size=\"50\" name=\"publisher\" value=\"\" /></td>",
-    "    </tr>",
-    "    <tr>",
-    "        <td>Jahr</td>",
-    "        <td><input type=\"text\" size=\"50\" name=\"year\" value=\"\" /></td>",
-    "    </tr>",
-    "    <tr>",
-    "        <td>Schlagw&ouml;rter</td>",
-    "        <td><input type=\"text\" size=\"50\" name=\"tags\" value=\"\" /></td>",
-    "    </tr>",
-    "</table>",
+    "{{/fields}}"].join('\n');
+
+var itemDetailsMustasheFoot = [ "</table>",
     "<input type=\"submit\" value=\"Speichern\" />",
     "</form>",
     "</div>"].join('\n');
 
+function renderAndBindDocToForm( doc ) {
+    var type = types[ doc.type ];
+
+    var fields = [];
+    for( var idx in type.fields ) {
+        if( idx ) {
+            fields[ idx ] = { id : type.fields[idx], name : type.fieldNames[ type.fields[idx] ] };
+        }
+    }
+
+    var html = $.mustache( itemDetailsMustacheHead, { divId : type.divSelector, formId : type.formSelector } );
+    html += $.mustache( itemDetailsMustasheRows, { fields : fields } );
+    html += itemDetailsMustasheFoot;
+    $("#content").append( html  );
+
+    var couchDbDetails = {
+        id : doc._id,
+        fields : type["fields"],
+        template : type["template"],
+        onLoad : function(doc) {
+            if( doc.tags ) {
+                doc.tags = doc.tags.join(" ");
+            }
+            if( doc.authors ) {
+                doc.authors = doc.authors.join(";");
+            }
+        },
+        beforeSave : function(doc) {
+            if(!doc.created_at) {
+                doc.created_at = new Date();
+            }
+            if(doc.tags) {
+                doc.tags = $.trim(doc.tags);
+                doc.tags = doc.tags.split(" ");
+                for(var idx in doc.tags) {
+                    doc.tags[idx] = $.trim(doc.tags[idx]);
+                }
+            }
+            if(doc.authors) {
+                doc.authors = doc.authors.split(";");
+                for(var idx in doc.authors) {
+                    doc.authors[idx] = $.trim(doc.authors[idx]);
+                }
+            }
+        }
+    };
+
+    $.CouchApp( function(app) {
+        var postForm = app.docForm( "#"+type["formSelector"], couchDbDetails );
+    });
+
+    $( "#"+type["divSelector"] ).show();
+}
+
 // function to show the item's details
 function showItemDetails( docid ) {
 
-    disableAllDisplays();
-    $("#content").append( bookDetailsHTML );
+    $("#content").empty();
 
     $.CouchApp( function( app ) {
         app.db.openDoc( docid, { 
             success : function(doc) {
-
-                var type = types[ doc.type ];
-
-                $.CouchApp( function(app) {
-                var postForm = app.docForm( type["formSelector"], {
-                    id : doc._id,
-                    fields : type["fields"],
-                    template : type["template"],
-                    onLoad : function(doc) {
-                        doc.tags = doc.tags.join(" ");
-                        doc.authors = doc.authors.join(";");
-                    },
-                    beforeSave : function(doc) {
-                        if(!doc.created_at) {
-                            doc.created_at = new Date();
-                        }
-                        if(doc.tags) {
-                            doc.tags = $.trim(doc.tags);
-                            doc.tags = doc.tags.split(" ");
-                            for(var idx in doc.tags) {
-                                doc.tags[idx] = $.trim(doc.tags[idx]);
-                            }
-                        }
-                        if(doc.authors) {
-                            doc.authors = doc.authors.split(";");
-                            for(var idx in doc.authors) {
-                                doc.authors[idx] = $.trim(doc.authors[idx]);
-                            }
-                        }
-                    }
-                    });
-                });
-
-                $( type["divSelector"] ).show();
+                renderAndBindDocToForm( doc );
             }
         });
     });
 }
 
-var newestListHTML = "<div id=\"listResult\"></div>";
+/* End:
+ *
+ * Methods and variables regarding item details.
+ *
+ */
+
+
+
+/* Begin:
+ *
+ * Methods and variables regarding the list of newest items.
+ *
+ */
+
+var newestItemsMustache = "<p><a class=\"link\" rel=\"history\" href=\"#docId{{docid}}\">{{title}}, {{author}}</a></p>";
 
 // function to list the newest items
 function showNewestItems() {
-    disableAllDisplays();
-    $("#content").append( newestListHTML );
+    $("#content").empty();
+    $("#content").append( $.mustache( empyDivMustache, { divid : "listResult" } ) );
 
     $.CouchApp( function( app ) {
-        $("#listResult").empty();
         var options = {};
         options.limit = 10;
+        options.descending = true;
         options.success = function( data ) {
             data.rows.map( function( row ) {
                 var view = {
@@ -142,6 +206,64 @@ function showNewestItems() {
         };
         app.view( "itemsByDate", options );
     });
-    $("#listResult").show();
+}
+/* End:
+ *
+ * Methods and variables regarding the list of newest items.
+ *
+ */
+
+/* Begin:
+ *
+ * Methods and variables regarding the tag cloud.
+ *
+ */
+
+var tagsMustache = ["{{#tags}}",
+    "<a style=\"font-size:{{{count}}}px\" href=\"#tag/{{{tag_uri}}}\">{{tag}}</a>",
+    "{{/tags}}"].join('\n');
+
+function showTagCloud() {
+    $("#content").empty();
+    $("#content").append( $.mustache( empyDivMustache, { divid : "tagcloud" } ) );
+
+    $.CouchApp( function( app ) {
+        var options = {};
+        options.group = true;
+        options.success = function( data ) {
+            var tags = data.rows.map( function( row ) {
+                return {
+                    tag : row.key,
+                    tag_uri : encodeURIComponent( row.key ),
+                    count : row.value * 10
+                };
+            });
+            $("#tagcloud").append( $.mustache( tagsMustache, { tags : tags } ) );
+        };
+        app.view( "tagcloud", options );
+    });
 }
 
+/* End:
+ *
+ * Methods and variables regarding the tag cloud.
+ *
+ */
+
+/*
+ * Begin:
+ *
+ * Methods and variables regarding the tag filtering.
+ *
+ */
+
+function showItemsByTags( tags ) {
+    $("#content").empty();
+    $("#correct").append( $.mustache ( empyDivMustache, { divid : "listResult" } ) );
+}
+/*
+ * Begin:
+ *
+ * Methods and variables regarding the tag filtering.
+ *
+ */
